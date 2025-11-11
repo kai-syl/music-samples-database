902f2674c909d6bcd77c63022d8fcefb85284e49
Kai R <kai@kaisyl.net>
2025-11-11 06:21:13 +0000

    Add create/read functionality with ORM for the artist_song many-to-many relationship

--- orm_flask_app/app.py
+++ orm_flask_app/app.py
@@ -12,19 +12,27 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{hos
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+artist_song = db.Table('artist_song',
+    db.Column('artist_id', db.Integer, db.ForeignKey('artist.artist_id'), primary_key=True),
+    db.Column('song_id', db.Integer, db.ForeignKey('song.song_id'), primary_key=True)
+)
+
 class Artist(db.Model):
+    __tablename__ = 'artist'
     artist_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     nationality = db.Column(db.String)
     genre = db.Column(db.String)
 
 class Song(db.Model):
+    __tablename__ = 'song'
     song_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     key = db.Column(db.String)
     bpm = db.Column(db.String)
     genre = db.Column(db.String)
     ISRC = db.Column(db.String)
+    artists = db.relationship("Artist", secondary=artist_song, backref='songs', lazy='dynamic')
 
 @app.route('/')
 def index():
@@ -37,6 +45,12 @@ def insertArtistForm():
 @app.route('/insertsong')
 def insertSongForm():
     return render_template("insertform.html", t="song")
+
+@app.route('/insertartistsong')
+def insertArtistSongForm():
+    artists = Artist.query.all()
+    songs = Song.query.all()
+    return render_template('insertform.html', t="artist_song", artists=artists, songs=songs)
 
 @app.route('/newartist')
 def insertArtist():
@@ -66,15 +80,38 @@ def insertSong():
         db.session.commit()
         return redirect("read?t=song")
 
+@app.route('/newartistsong')
+def insertArtistSong():
+    artist_id = request.args.get('artist_id')
+    song_id = request.args.get('song_id')
+    if artist_id is not None and song_id is not None:
+        artist = Artist.query.get(artist_id)
+        song = Song.query.get(song_id)
+        song.artists.append(artist)
+        db.session.commit()
+        return redirect('/read?t=song')
+    else:
+        return redirect('insertartistsong')
+
 @app.route('/read')
 def read():
     table = request.args.get('t')
+    song_id = request.args.get('song_id')
+    artist_id = request.args.get('a')
     if table == 'song':
-        songs = Song.query.all()
-        output = render_template("showsongs.html", songs=songs)
+        if song_id is not None:
+            song = Song.query.get(song_id)
+            output = render_template("showsong.html", song=song)
+        else:
+            songs = Song.query.all()
+            output = render_template("showsongs.html", songs=songs)
     elif table == 'artist':
-        artists = Artist.query.all()
-        output = render_template("showartists.html", artists=artists)
+        if artist_id is not None:
+            artist = Artist.query.get(artist_id)
+            output = render_template("showartist.html", artist=artist)
+        else:
+            artists = Artist.query.all()
+            output = render_template("showartists.html", artists=artists)
     return output
 
 @app.route('/updateform')
@@ -87,10 +124,12 @@ def updateArtistForm():
             return render_template("updateform.html", artist=artist)
     elif request.args.get('song_id') is not None:
         song = Song.query.get(request.args.get('song_id'))
+        songArtists = [x.artist_id for x in song.artists]
+        otherArtists = Artist.query.filter(Artist.artist_id.not_in(songArtists))
         if song is None:
             return redirect('read?t=song')
         else:
-            return render_template("updateform.html", song=song)
+            return render_template("updateform.html", song=song, otherArtists=otherArtists)
     else:
         return redirect("/")
 
@@ -106,14 +145,19 @@ def updateArtist():
 
 @app.route('/updatesong')
 def updateSong():
-    song = Song.query.get(request.args.get('song_id'))
+    song_id = request.args.get('song_id')
+    song = Song.query.get(song_id)
     song.title = request.args.get('title')
     song.key = request.args.get('key')
     song.genre = request.args.get('genre')
     song.bpm = request.args.get('bpm')
     song.ISRC = request.args.get('ISRC')
+    # stand-in until I write full artist_song update capability, not just create:
+    artist = Artist.query.get(request.args.get('artist_id'))
+    song.artists.append(artist)
+
     db.session.commit()
-    return redirect('read?t=song')
+    return redirect(f'read?t=song&song_id={song_id}')
 
 @app.route('/delete')
 def delete():
